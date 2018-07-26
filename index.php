a736720675b4ea6489bd42146ebd11162755ebb7
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 10:23:08 +0200

    exercice4 git commit -m exercice4 git commit -m exercice4

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice4</title>
+  </head>
+  <body>
+    <?php
+      $string = 'MY';
+      echo $string;
+      $int = 92;
+      echo $int;
+      $float = 27.10;
+      echo $float;
+      $bool = true;
+      echo $bool;
+     ?>
+  </body>
+</html>
